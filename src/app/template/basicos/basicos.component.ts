fe81ef5ff5c1e896697667fa2cf19c947535db8f
davidcrc <david_drcr@hotmail.com>
2021-12-22 17:36:20 -0500

    16-8. Template FormsModule

--- src/app/template/basicos/basicos.component.ts
+++ src/app/template/basicos/basicos.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
   templateUrl: './basicos.component.html',
   styles: [],
 })
-export class BasicosComponent implements OnInit {
+export class BasicosComponent {
   constructor() {}
 
-  ngOnInit(): void {}
+  guardar(miFormulario: NgForm) {
+    console.log(miFormulario.value);
+  }
 }
