92ed7fd12680cf0c8a4a08b2cb428df250ded56d
davidcrc <david_drcr@hotmail.com>
2021-12-24 12:57:21 -0500

    16-15. Agregar elementos de forma din mica

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,11 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Favorito {
+  id: number;
+  nombre: string;
+}
+
+interface Persona {
+  nombre: string;
+  favoritos: Favorito[];
+}
+
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
   styles: [],
 })
 export class DinamicosComponent {
+  persona: Persona = {
+    nombre: 'Persona',
+    favoritos: [
+      {
+        id: 1,
+        nombre: 'Favorito 1',
+      },
+      {
+        id: 2,
+        nombre: 'Favorito 2',
+      },
+    ],
+  };
   guardar() {
     console.log('guardar');
   }
