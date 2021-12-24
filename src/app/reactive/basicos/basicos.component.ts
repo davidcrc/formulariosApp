a8d47cc02e574d4d45cc7548a17fef430a5c3814
davidcrc <david_drcr@hotmail.com>
2021-12-24 16:48:23 -0500

    17-5. FormBuilder

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -8,8 +8,17 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class BasicosComponent {
   // nombre => sera utilizacon con 'formControlName' en el html
-  miFormulario: FormGroup = new FormGroup({
-    nombre: new FormControl('Un objeto cualquiera'),
+  // miFormulario: FormGroup = new FormGroup({
+  //   nombre: new FormControl('Un objeto cualquiera'),
+  //   precio: new FormControl(0),
+  //   existencias: new FormControl(5),
+  // });
+
+  // Ora manera de crear lo anterior
+  miFormulario: FormGroup = this.fb.group({
+    nombre: ['Un objeto cualquiera'],
+    precio: [0],
+    existencias: [0],
   });
-  constructor() {}
+  constructor(private fb: FormBuilder) {}
 }
