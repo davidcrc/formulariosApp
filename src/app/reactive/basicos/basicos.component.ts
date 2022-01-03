19912b585a0969c559e3e6769a7e3ac433200a0c
davidcrc <david_drcr@hotmail.com>
2022-01-03 09:14:17 -0500

    17-8. Submit del formulario

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -11,7 +11,7 @@ import {
   templateUrl: './basicos.component.html',
   styles: [],
 })
-export class BasicosComponent {
+export class BasicosComponent implements OnInit {
   // nombre => sera utilizacon con 'formControlName' en el html
   // miFormulario: FormGroup = new FormGroup({
   //   nombre: new FormControl('Un objeto cualquiera'),
@@ -29,10 +29,31 @@ export class BasicosComponent {
   });
   constructor(private fb: FormBuilder) {}
 
+  ngOnInit(): void {
+    // Valores por defecto mejor usar el reset
+    // ya que el setValue requiere setear todos los campos
+    this.miFormulario.reset({
+      nombre: 'Alguna cosa',
+      // precio: 0,
+      existencias: 0,
+    });
+  }
+
   campoValido(campo: string) {
     return (
       this.miFormulario.controls[campo]?.errors &&
       this.miFormulario.controls[campo]?.touched
     );
+  }
+
+  guardar() {
+    if (this.miFormulario.invalid) {
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.miFormulario.value);
+
+    this.miFormulario.reset();
   }
 }
